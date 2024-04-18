d734b42b8ae051128ca4f463cfcc7db69069116c
Aditya8757343114 <166206608+Aditya8757343114@users.noreply.github.com>
2024-04-18 09:58:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,9 @@ load_dotenv()
 API_ID = int(getenv("API_ID", None))
 API_HASH = getenv("API_HASH", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
-OWNER_ID = int(getenv("OWNER_ID", 6973766632"))
+OWNER_ID = int(getenv("OWNER_ID", "6696377088"))
 MONGO_URL = getenv("MONGO_URL", None)
 SUPPORT_GRP = getenv("SUPPORT_GRP", "music_support_group")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "music_support_channel")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "@Matlabi_insaan")
-
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Secret_boy_here")
 
